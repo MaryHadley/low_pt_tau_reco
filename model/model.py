8bf578e6d56ae24e78635107a4fee462963281c6
Willem <willem@gotenzo.com>
2019-08-05 14:04:47 +0200

    Simplify model file

--- model/model.py
+++ model/model.py
@@ -41,28 +41,6 @@ antitau_label_names = [
     b'antineutrino_phi',
 ]
 
-
-def normalize_sin_cos(value):
-    if value > 1:
-        return 1
-    elif value < -1:
-        return -1
-    return value
-
-
-def get_radians_from_sin_cos(sin_value, cos_value):
-    sin_value = normalize_sin_cos(sin_value)
-    cos_value = normalize_sin_cos(cos_value)
-    if sin_value > 0 and cos_value > 0:
-        return (np.arcsin(sin_value) + np.arccos(cos_value)) / 2
-    elif sin_value > 0 > cos_value:
-        return ((np.pi - np.arcsin(sin_value)) + np.arccos(cos_value)) / 2
-    elif sin_value < 0 < cos_value:
-        return (np.arcsin(sin_value) - np.arccos(cos_value)) / 2
-    elif sin_value < 0 and cos_value < 0:
-        return ((- np.arccos(cos_value)) - (np.pi + np.arcsin(sin_value))) / 2
-
-
 file = uproot.open('momentum_vector_data100k.root')['tree']
 
 tau_features = []
@@ -141,18 +119,6 @@ antitau_features_test = antitau_features[int(0.9 * antitau_features.shape[0]):, 
 antitau_labels = np.transpose(np.array(antitau_labels))
 antitau_labels_train = antitau_labels[0: int(0.9 * antitau_labels.shape[0]), :]
 antitau_labels_test = antitau_labels[int(0.9 * antitau_labels.shape[0]):, :]
-
-branches = [
-    'tau_mass',
-    'antitau_mass',
-    'epsilon_mass'
-]
-file_out = ROOT.TFile('nn_tau_antitau_mass_with_neutrinos.root', 'recreate')
-file_out.cd()
-
-tofill = OrderedDict(zip(branches, [-99.] * len(branches)))
-
-masses = ROOT.TNtuple('tree', 'tree', ':'.join(branches))
 
 
 def create_model():
@@ -268,74 +234,4 @@ anti_pred = antitau_model.predict(
 
 tau_model.save('tau_model.hdf5')
 antitau_model.save('antitau_model.hdf5')
-
-for event in range(pred.shape[0]):
-    tau_lorentz = TLorentzVector()
-    for index in range(0, pred.shape[1], 4):
-        lorentz = TLorentzVector()
-        phi = get_radians_from_sin_cos(
-            pred[event][index + 2],
-            pred[event][index + 3]
-        )
-        lorentz.SetPtEtaPhiM(
-            pred[event][index],
-            pred[event][index + 1],
-            phi,
-            0
-        )
-        tau_lorentz += lorentz
-    for index in range(0, tau_features_test.shape[1], 4):
-        lorentz = TLorentzVector()
-        phi = get_radians_from_sin_cos(
-            tau_features_test[event][index + 2],
-            tau_features_test[event][index + 3]
-        )
-        lorentz.SetPtEtaPhiM(
-            tau_features_test[event][index],
-            tau_features_test[event][index + 1],
-            phi,
-            0.139
-        )
-        tau_lorentz += lorentz
-
-    antitau_lorentz = TLorentzVector()
-    for index in range(0, anti_pred.shape[1], 4):
-        lorentz = TLorentzVector()
-        phi = get_radians_from_sin_cos(
-            anti_pred[event][index + 2],
-            anti_pred[event][index + 3]
-        )
-        lorentz.SetPtEtaPhiM(
-            anti_pred[event][index],
-            anti_pred[event][index + 1],
-            phi,
-            0
-        )
-        antitau_lorentz += lorentz
-    for index in range(0, tau_features_test.shape[1], 4):
-        lorentz = TLorentzVector()
-        phi = get_radians_from_sin_cos(
-            antitau_features_test[event][index + 2],
-            antitau_features_test[event][index + 3]
-        )
-        lorentz.SetPtEtaPhiM(
-            antitau_features_test[event][index],
-            antitau_features_test[event][index + 1],
-            phi,
-            0.139
-        )
-        antitau_lorentz += lorentz
-
-    epsilon_lorentz = tau_lorentz + antitau_lorentz
-
-    tofill['tau_mass'] = tau_lorentz.M()
-    tofill['antitau_mass'] = antitau_lorentz.M()
-    tofill['epsilon_mass'] = epsilon_lorentz.M()
-    masses.Fill(array('f', tofill.values()))
-
-file_out.cd()
-masses.Write()
-file_out.Close()
-
-
 
