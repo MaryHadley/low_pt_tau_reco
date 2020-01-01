508e6341591435d12f1b6204e3ae77d143cf766b
Mary Hadley <mary.hadley@cern.ch>
2020-01-01 16:44:53 -0600

    function to unrotate from visible tau mass points in eta = 0 included in complete form
    function to unrotate from visible tau mass points in eta = 0 has been sanity-checked and tested, found to be ok

--- preprocessing/changeFrames.py
+++ preprocessing/changeFrames.py
@@ -28,8 +28,8 @@ def rotateToVisTauMomPointsInEtaEqualsZero(tau_orig_theta, tau_orig_phi, orig_fo
 
  # protection to make sure things that really are zero get set to 0 and not 10^-17 or something 
     for element in np.nditer(rotMatrix, op_flags=['readwrite']):
-         if abs(element) < 10.**(-10):
-             element[...] = 0
+        if abs(element) < 10.**(-10):
+            element[...] = 0
 #    print "rotMatrix after is:", rotMatrix        
     tmp_Px = orig_four_vec_to_rotate.Px()
     tmp_Py = orig_four_vec_to_rotate.Py()
@@ -58,11 +58,34 @@ def unrotateFromVisTauMomPointsInEtaEqualsZero(tau_orig_theta, tau_orig_phi, rot
      [0,                         np.cos(tau_orig_theta),                                  np.sin(tau_orig_theta)]                                                                                                                        
     
     ])
+    
+    # protection to make sure things that really are zero get set to 0 and not 10^-17 or something
+    
+    for unElement in np.nditer(unrotMatrix, op_flags=['readwrite']):
+        if abs(unElement) <  10.**(-10):
+            unElement[...] = 0
+    
+    tmp_Px = rot_four_vec_to_unrotate.Px()
+    tmp_Py = rot_four_vec_to_unrotate.Py()
+    tmp_Pz = rot_four_vec_to_unrotate.Pz()
+    tmp_E  = rot_four_vec_to_unrotate.E()  #there is apparently not a convenient SetPxPyPzM method, so we will use SetPxPyPzE down below
+
+    tmp_PxPyPz_vec_to_mult = [[tmp_Px], [tmp_Py], [tmp_Pz]]
+    
+    tmp_unrotated_PxPyPz_vec = np.dot(unrotMatrix, tmp_PxPyPz_vec_to_mult) #matrix multiplication of unrotMatrix times the rotated vector...the rotated vector is rotMatrix times original vector, therefore what we are doing here is unrotMatrix * rotMatrix * orig vector, which is identity matrix times orig vector, which gives us back orig vector
+    
+    tmp_unrotated_Px = tmp_unrotated_PxPyPz_vec[0]
+    tmp_unrotated_Py = tmp_unrotated_PxPyPz_vec[1]
+    tmp_unrotated_Pz = tmp_unrotated_PxPyPz_vec[2]
+    
+    Global_4vec = ROOT.TLorentzVector()
+    Global_4vec.SetPxPyPzE(tmp_unrotated_Px, tmp_unrotated_Py, tmp_unrotated_Pz, tmp_E)
+    
+    return Global_4vec
 
 
 
-
-
+    
 ##### test #####
 
 v = TLorentzVector()
@@ -89,3 +112,18 @@ newPhi = toPrint.Phi()
 newEta = toPrint.Eta()
 
 print "new Px, Py, Pz, E, M, Theta, Phi, Eta:", newPx, newPy, newPz, newE, newM, newTheta, newPhi, newEta
+
+v2 = TLorentzVector()
+v2.SetPxPyPzE(8.881784197e-16,22.1419273613,0.0,22.1777103583)
+
+toPrint2 = unrotateFromVisTauMomPointsInEtaEqualsZero(tau_orig_theta_test, tau_orig_phi_test, v2)
+
+newPx2 = toPrint2.Px()
+newPy2 = toPrint2.Py()
+newPz2 = toPrint2.Pz()
+newE2 =   toPrint2.E()
+newM2 = toPrint2.M()
+newTheta2 = toPrint2.Theta()
+newPhi2   = toPrint2.Phi()
+newEta2 = toPrint2.Eta()
+print "new Px2, Py2, Pz2, E, M, Theta2, Phi2, Eta2:", newPx2, newPy2, newPz2, newE2, newM2, newTheta2, newPhi2, newEta2
